7acd45510b0bc1de2f8ceceadf15f90e5c0ff2a2
BaoJay <giabao.nguyenhoang1001@gmail.com>
2026-06-19 06:03:55 +0000

    Setup maxDistance, minDistance, initialDistance ban đầu

--- src/viewer/createViewer.ts
+++ src/viewer/createViewer.ts
@@ -429,13 +429,18 @@ export function createViewer({
     // fitDistance là khoảng cách từ camera đến tâm orbit, để toàn bộ model nằm trong view
     const fitDistance = fitPadding * Math.max(verticalFitDistance, horizontalFitDistance);
 
+    // Giới hạn khoảng cách camera để tránh zoom quá gần hoặc quá xa
+    const minDistance = fitDistance * 0.1;
+    const maxDistance = fitDistance;
+    const initialDistance = maxDistance/2;
+
     // Hướng đặt camera ban đầu so với tâm orbit, lấy từ config viewer-config.ts, fitDirection: [1.25, 0.72, 1.4]
     // .normalize() biến vector này thành vector đơn vị, nên độ lớn không quan trọng, chỉ quan trọng tỉ lệ giữa x/y/z
     const fitDirection = new Vector3(...config.camera.fitDirection).normalize();
 
     // nextPosition là vị trí camera ban đầu, thực tế trong world space
     // nextPosition = hướng * khoảng cách + tâm orbit
-    const nextPosition = fitDirection.multiplyScalar(fitDistance * 0.5).add(orbitTarget);
+    const nextPosition = fitDirection.multiplyScalar(initialDistance).add(orbitTarget);
 
     camera.position.copy(nextPosition);
     // vật thể gần camera hơn khoảng này sẽ không được render.
@@ -446,8 +451,8 @@ export function createViewer({
 
     // Trục orbit thật nằm ở đây
     controls.target.copy(orbitTarget);
-    controls.minDistance = fitDistance * 0.1;
-    controls.maxDistance = fitDistance;
+    controls.minDistance = minDistance;
+    controls.maxDistance = maxDistance;
     controls.update();
 
     // Lưu trạng thái view ban đầu để reset khi bấm button reset
